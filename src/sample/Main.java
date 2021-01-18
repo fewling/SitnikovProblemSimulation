22fee1f7a412a93d535409a3ab4ae6c04c6fe446
Fewling <felixwong875@gmail.com>
2021-01-18 20:15:36 +0800

    Optimizations (warnings etc.)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,14 +3,10 @@ package sample;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Point3D;
 import javafx.scene.Camera;
-import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -23,13 +19,6 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 
-    private final Sphere sphere1 = new Sphere();
-    private final Sphere sphere2 = new Sphere();
-    private final Sphere sphere3 = new Sphere();
-    private final double timeStep = 0.75;
-    private final int GM = 40000;
-    private double powerOfPosition; // Just making the calculation easier to read
-
     // initial condition:
     private double xOfSphere1 = -300, yOfSphere1 = 0;
     private double xOfSphere2 = 300, yOfSphere2 = 0;
@@ -37,32 +26,32 @@ public class Main extends Application {
     private double vxOfSphere2 = 0, vyOfSphere2 = -5;
     private final double xOfSphere3 = 0;
     private final double yOfSphere3 = 0;
-    private double zOfSphere3 = 0; // Customizable
-    private double vzOfSphere3 = 3;
+    private double zOfSphere3 = 0;                  // Customizable
+    private double vzOfSphere3 = 3;                 // Customizable, but depends on zOfSphere, max when zOfSphere == 0
+    private static final double TIMESTEP = 0.75;    // Customizable
+    private static final double GM = 40000;         // Customizable, Gravitational Constant
+    private static final double LARGE_RADIUS = 20;  // Radius for M1 & M2
+    private static final double SMALL_RADIUS = 5;   // Radius for M3
 
-    private final int largeRadius = 20;
-    private final int smallRadius = 5;
 
-    private double powOfXOfSphere1;
-    private double powOfYOfSphere1;
-    private double powOfXOfSphere2;
-    private double powOfYOfSphere2;
-    private double powZOfSphere3;
-    private double lastX1, lastY1, lastX2, lastY2;
+    private final Sphere sphere1 = new Sphere();
+    private final Sphere sphere2 = new Sphere();
+    private final Sphere sphere3 = new Sphere();
+
+
+    private double lastX1, lastY1, lastX2, lastY2;  // Recording variables to draw tracks
     private Rotate rotateX, rotateY, rotateZ;
-    public static Camera camera = new PerspectiveCamera(true);
+    public static Camera camera = new PerspectiveCamera(true); // true --> eye fixed on (0, 0, 0)
     private Translate translate;
-    private Line line;
     private Pane pane;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
         // Create the bodies:
-        sphere1.setRadius(largeRadius);
-        sphere2.setRadius(largeRadius);
-        sphere3.setRadius(smallRadius);
-        line = new Line();
+        sphere1.setRadius(LARGE_RADIUS);
+        sphere2.setRadius(LARGE_RADIUS);
+        sphere3.setRadius(SMALL_RADIUS);
 
         // Setting the appearances:
         PhongMaterial material1 = new PhongMaterial();
@@ -90,7 +79,6 @@ public class Main extends Application {
         pane.getChildren().add(sphere1);
         pane.getChildren().add(sphere2);
         pane.getChildren().add(sphere3);
-        pane.getChildren().add(line);
 
         // Create scene to hold the pane and attach Camera
         Scene scene = new Scene(pane, 800, 800);
@@ -115,39 +103,19 @@ public class Main extends Application {
         camera.setFarClip(2000);
         ((PerspectiveCamera) camera).setFieldOfView(35);
 
+        // Camera control:
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             switch (event.getCode()) {
-                case A:
-                    translate.setX(translate.getX() - 5);
-                    break;
-                case D:
-                    translate.setX(translate.getX() + 5);
-                    break;
-                case W:
-                    translate.setY(translate.getY() + 5);
-                    break;
-                case S:
-                    translate.setY(translate.getY() - 5);
-                    break;
-                case LEFT:
-                    rotateZ.setAngle(rotateY.getAngle() + 5);
-                    break;
-                case RIGHT:
-                    rotateY.setAngle(rotateY.getAngle() - 5);
-                    break;
-                case UP:
-                    rotateX.setAngle(rotateX.getAngle() - 5);
-                    break;
-                case DOWN:
-                    rotateX.setAngle(rotateX.getAngle() + 5);
-                    break;
-                case Y:
-                    translate.setZ(translate.getZ() + 5);
-                    System.out.println("hi");
-                    break;
-                case H:
-                    translate.setZ(translate.getZ() - 5);
-                    break;
+                case A -> translate.setX(translate.getX() - 5);
+                case D -> translate.setX(translate.getX() + 5);
+                case W -> translate.setY(translate.getY() + 5);
+                case S -> translate.setY(translate.getY() - 5);
+                case LEFT -> rotateY.setAngle(rotateY.getAngle() + 5);
+                case RIGHT -> rotateY.setAngle(rotateY.getAngle() - 5);
+                case UP -> rotateX.setAngle(rotateX.getAngle() - 5);
+                case DOWN -> rotateX.setAngle(rotateX.getAngle() + 5);
+                case Y -> translate.setZ(translate.getZ() + 5);
+                case H -> translate.setZ(translate.getZ() - 5);
             }
         });
 
@@ -164,43 +132,40 @@ public class Main extends Application {
         lastX2 = xOfSphere2;
         lastY2 = yOfSphere2;
 
-        powOfXOfSphere1 = Math.pow(xOfSphere1, 2);
-        powOfYOfSphere1 = Math.pow(yOfSphere1, 2);
-        powerOfPosition = Math.pow(powOfXOfSphere1 + powOfYOfSphere1, 1.5);
+        // Just separating the calculation steps for M1
+        double powX1 = Math.pow(xOfSphere1, 2);
+        double powY1 = Math.pow(yOfSphere1, 2);
+        double powerOfPosition = Math.pow(powX1 + powY1, 1.5);
 
-        // Calculate v for M1:
-        vxOfSphere1 = vxOfSphere1 + ((-1) * GM / (2 * powerOfPosition)) * xOfSphere1 * timeStep;
+        // Calculate and update velocities and locations (M1):
+        vxOfSphere1 = vxOfSphere1 + ((-1) * GM / (2 * powerOfPosition)) * xOfSphere1 * TIMESTEP;
         xOfSphere1 = xOfSphere1 + vxOfSphere1;
 
-        // Calculate coordinates for M1:
-        vyOfSphere1 = vyOfSphere1 + ((-1) * GM / (2 * powerOfPosition)) * yOfSphere1 * timeStep;
+        vyOfSphere1 = vyOfSphere1 + ((-1) * GM / (2 * powerOfPosition)) * yOfSphere1 * TIMESTEP;
         yOfSphere1 = yOfSphere1 + vyOfSphere1;
 
-        /**********************************************/
 
-        powOfXOfSphere2 = Math.pow(xOfSphere2, 2);
-        powOfYOfSphere2 = Math.pow(yOfSphere2, 2);
-        powerOfPosition = Math.pow(powOfXOfSphere2 + powOfYOfSphere2, 1.5);
+        // Just separating the calculation steps for M2
+        double powX2 = Math.pow(xOfSphere2, 2);
+        double powY2 = Math.pow(yOfSphere2, 2);
+        powerOfPosition = Math.pow(powX2 + powY2, 1.5);
 
-        // Calculate v for M2:
-        vxOfSphere2 = vxOfSphere2 + ((-1) * GM / (2 * powerOfPosition)) * xOfSphere2 * timeStep;
+        // Calculate and update velocities and locations (M2):
+        vxOfSphere2 = vxOfSphere2 + ((-1) * GM / (2 * powerOfPosition)) * xOfSphere2 * TIMESTEP;
         xOfSphere2 = xOfSphere2 + vxOfSphere2;
 
-        // Calculate coordinates for M2:
-        vyOfSphere2 = vyOfSphere2 + ((-1) * GM / (2 * powerOfPosition)) * yOfSphere2 * timeStep;
+        vyOfSphere2 = vyOfSphere2 + ((-1) * GM / (2 * powerOfPosition)) * yOfSphere2 * TIMESTEP;
         yOfSphere2 = yOfSphere2 + vyOfSphere2;
 
-        /**********************************************/
 
-        double powX = Math.pow(xOfSphere2 - xOfSphere1, 2);
-        double powY = Math.pow(yOfSphere2 - yOfSphere1, 2);
-        powZOfSphere3 = Math.pow(zOfSphere3, 2);
-        powerOfPosition = Math.pow(powX + powY + powZOfSphere3, 1.5);
+        // Just separating the calculation steps for M3
+        double powXDiff = Math.pow(xOfSphere2 - xOfSphere1, 2);
+        double powYDiff = Math.pow(yOfSphere2 - yOfSphere1, 2);
+        double powZ3 = Math.pow(zOfSphere3, 2);
+        powerOfPosition = Math.pow(powXDiff + powYDiff + powZ3, 1.5);
 
-        // Calculate v for M3:
-        vzOfSphere3 = vzOfSphere3 + (-1) * ((4 * GM) / powerOfPosition) * zOfSphere3 * timeStep;
-
-        // Calculate coordinates for M3:
+        // Calculate and update velocities and locations (M3):
+        vzOfSphere3 = vzOfSphere3 + (-1) * ((4 * GM) / powerOfPosition) * zOfSphere3 * TIMESTEP;
         zOfSphere3 = zOfSphere3 + vzOfSphere3;
 
         move(xOfSphere1, yOfSphere1, xOfSphere2, yOfSphere2, zOfSphere3);
@@ -208,26 +173,29 @@ public class Main extends Application {
 
     private void move(double x1, double y1, double x2, double y2, double z) {
 
-        // draw tracks:
+        // draw tracks of M1 & M2:
         pane.getChildren().add(new Line(lastX1, lastY1, x1, y1));
         pane.getChildren().add(new Line(lastX2, lastY2, x2, y2));
 
+        // Move M1
         sphere1.translateXProperty().set(x1);
         sphere1.translateYProperty().set(y1);
 
+        // Move M2
         sphere2.translateXProperty().set(x2);
         sphere2.translateYProperty().set(y2);
 
+        // Move M3
         sphere3.translateXProperty().set(xOfSphere3);
         sphere3.translateYProperty().set(yOfSphere3);
         sphere3.translateZProperty().set(z);
 
-        System.out.print("TranslateX: " + translate.getX());
-        System.out.print(", TranslateY: " + translate.getY());
-        System.out.print(", TranslateZ: " + translate.getZ());
-        System.out.print(", RotateX: " + rotateX.getAngle());
-        System.out.print(", RotateY: " + rotateY.getAngle());
-        System.out.println(", RotateZ: " + rotateZ.getAngle());
+//        System.out.print("TranslateX: " + translate.getX());
+//        System.out.print(", TranslateY: " + translate.getY());
+//        System.out.print(", TranslateZ: " + translate.getZ());
+//        System.out.print(", RotateX: " + rotateX.getAngle());
+//        System.out.print(", RotateY: " + rotateY.getAngle());
+//        System.out.println(", RotateZ: " + rotateZ.getAngle());
     }
 
     public static void main(String[] args) {
