f40add4caf9a43d764c62692860bd6cbdd64d5c8
fewling <felixwong875@gmail.com>
2021-01-20 22:30:09 +0800

    1) Corrected calculations 2) Added coordinates labels 3) Small changes to camera angle

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Camera;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -20,18 +22,23 @@ import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class Main extends Application {
 
 
-    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_WIDTH = 1000;
     public static final int SCREEN_HEIGHT = 800;
+    private static final double SUBSCENE_WIDTH = 800;
+    private static final double SUBSCENE_HEIGHT = 800;
     public static final int FAR_CLIP = 20000;           // Maximum range of camera view
     public static final double NEAR_CLIP = 0.1;         // Minimum range of camera view
 
     // Initial conditions:
-    private static final double TIMESTEP = 1;           // Customizable
+    private static final double TIMESTEP = 0.01;         // Customizable
     private static final double GM = 40000;             // Customizable, Gravitational Constant (M1 &2)
-    private static final double Gm = 200;               // Customizable, Gravitational Constant (M3)
+    private static final double Gm = 0;                 // Customizable, Gravitational Constant (M3)
     private static final double LARGE_RADIUS = 20;      // Radius for M1 & M2
     private static final double SMALL_RADIUS = 5;       // Radius for M3
     private double xOfSphere1 = -300, yOfSphere1 = 0;
@@ -49,11 +56,31 @@ public class Main extends Application {
     private final Sphere sphere3 = new Sphere();
 
 
-    private double lastX1, lastY1, lastX2, lastY2;  // Recording variables to draw tracks
+    private double lastX1, lastY1, lastX2, lastY2, lastZ3;  // Recording variables to draw tracks
     private Rotate rotateX, rotateY, rotateZ;
     public static Camera camera = new PerspectiveCamera(true); // true --> eye fixed on (0, 0, 0)
     private Translate translate;
     private Pane displayPane;
+
+    private final Label currentSphere1XLabel = new Label();
+    private final Label currentSphere1YLabel = new Label();
+    private final Label currentSphere2XLabel = new Label();
+    private final Label currentSphere2YLabel = new Label();
+    private final Label currentSphere3ZLabel = new Label();
+
+    private double minX1 = xOfSphere1, maxX1;
+    private double minX2, maxX2 = xOfSphere2;
+    private double maxZ3 = zOfSphere3;
+    private final Label minSphere1XLabel = new Label();
+    private final Label maxSphere1XLabel = new Label();
+    private final Label minSphere2XLabel = new Label();
+    private final Label maxSphere2XLabel = new Label();
+    private final Label maxSphere3ZLabel = new Label();
+
+    private int cycleCount = 0, yCount = 0;
+    private final Label cycleCountLabel = new Label();
+    private Timeline animation;
+    private boolean touchedYAxis = true;
 
     @Override
     public void start(Stage primaryStage) {
@@ -80,7 +107,7 @@ public class Main extends Application {
         sphere3.setMaterial(material3);
 
         // Setting the animation
-        Timeline animation = new Timeline(new KeyFrame(Duration.millis(10), e -> calculate()));
+        animation = new Timeline(new KeyFrame(Duration.millis(1), e -> calculate()));
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play();
 
@@ -89,14 +116,55 @@ public class Main extends Application {
         displayPane.getChildren().add(sphere1);
         displayPane.getChildren().add(sphere2);
         displayPane.getChildren().add(sphere3);
+        displayPane.getChildren().add(new Line(SUBSCENE_WIDTH, 0, -1 * SUBSCENE_WIDTH, 0));
+        displayPane.getChildren().add(new Line(0, SUBSCENE_HEIGHT, 0, -1 * SUBSCENE_HEIGHT));
 
         // Create subscene to hold the displayPane and attach camera
-        SubScene subScene = new SubScene(displayPane, 600, 600);
+        SubScene subScene = new SubScene(displayPane, SUBSCENE_WIDTH, SUBSCENE_HEIGHT);
         subScene.setCamera(camera);
 
         // Create splitPane to hold both controls and the subscene with displayPane
         SplitPane splitPane = new SplitPane();
         VBox leftControl = new VBox();
+
+        Button topViewBtn = new Button("Top View");
+        Button tiltViewBtn = new Button("Tilt View");   // default view
+        topViewBtn.setDisable(false);
+        tiltViewBtn.setDisable(true);
+
+        topViewBtn.setOnAction(actionEvent -> {
+            camera.getTransforms().addAll(
+                    rotateX = new Rotate(290, Rotate.X_AXIS),
+                    rotateY = new Rotate(0, Rotate.Y_AXIS),
+                    rotateZ = new Rotate(0, Rotate.Z_AXIS),
+                    translate = new Translate(-60, -935, -1030));
+            topViewBtn.setDisable(true);
+            tiltViewBtn.setDisable(false);
+        });
+
+        tiltViewBtn.setOnAction(actionEvent -> {
+            camera.getTransforms().addAll(
+                    rotateX = new Rotate(70, Rotate.X_AXIS),
+                    rotateY = new Rotate(0, Rotate.Y_AXIS),
+                    rotateZ = new Rotate(0, Rotate.Z_AXIS),
+                    translate = new Translate(60, 935, -1030));
+            topViewBtn.setDisable(false);
+            tiltViewBtn.setDisable(true);
+        });
+
+        leftControl.getChildren().add(topViewBtn);
+        leftControl.getChildren().add(tiltViewBtn);
+        leftControl.getChildren().add(currentSphere1XLabel);
+        leftControl.getChildren().add(currentSphere1YLabel);
+        leftControl.getChildren().add(currentSphere2XLabel);
+        leftControl.getChildren().add(currentSphere2YLabel);
+        leftControl.getChildren().add(currentSphere3ZLabel);
+        leftControl.getChildren().add(minSphere1XLabel);
+        leftControl.getChildren().add(maxSphere1XLabel);
+        leftControl.getChildren().add(maxSphere2XLabel);
+        leftControl.getChildren().add(minSphere2XLabel);
+        leftControl.getChildren().add(maxSphere3ZLabel);
+        leftControl.getChildren().add(cycleCountLabel);
         splitPane.getItems().addAll(leftControl, subScene);
 
         // Create a main scene to hold the splitPane
@@ -110,11 +178,9 @@ public class Main extends Application {
         // MOVE CAMERA
         camera.getTransforms().addAll(
                 rotateX = new Rotate(70, Rotate.X_AXIS),
-//                rotateX = new Rotate(0, Rotate.X_AXIS),
                 rotateY = new Rotate(0, Rotate.Y_AXIS),
                 rotateZ = new Rotate(0, Rotate.Z_AXIS),
-                translate = new Translate(0, 935, -655));
-//                translate = new Translate(0, 0, 0));
+                translate = new Translate(60, 935, -1030));
 
         // Move camera to a good view
         camera.translateZProperty().set(-1000);
@@ -152,6 +218,15 @@ public class Main extends Application {
         lastY1 = yOfSphere1;
         lastX2 = xOfSphere2;
         lastY2 = yOfSphere2;
+        lastZ3 = zOfSphere3;
+
+
+        // record current velocities:
+        double lastVXOfSphere1 = vXOfSphere1;
+        double lastVYOfSphere1 = vYOfSphere1;
+        double lastVXOfSphere2 = vXOfSphere2;
+        double lastVYOfSphere2 = vYOfSphere2;
+        double lastVZOfSphere3 = vZOfSphere3;
 
         // Just separating the calculation steps for M1
         double powX1 = Math.pow(xOfSphere1, 2);
@@ -161,11 +236,11 @@ public class Main extends Application {
         double powXYZ = Math.pow(powX1 + powY1 + powZ3, 1.5);
 
         // Calculate and update velocities and locations (M1):
-        vXOfSphere1 = vXOfSphere1 + (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * xOfSphere1 * TIMESTEP;
-        xOfSphere1 = xOfSphere1 + vXOfSphere1;
+        vXOfSphere1 += (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * xOfSphere1 * TIMESTEP;
+        xOfSphere1 += lastVXOfSphere1 * TIMESTEP;
 
-        vYOfSphere1 = vYOfSphere1 + (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * yOfSphere1 * TIMESTEP;
-        yOfSphere1 = yOfSphere1 + vYOfSphere1;
+        vYOfSphere1 += (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * yOfSphere1 * TIMESTEP;
+        yOfSphere1 += lastVYOfSphere1 * TIMESTEP;
 
 
         // Just separating the calculation steps for M2
@@ -176,10 +251,10 @@ public class Main extends Application {
 
         // Calculate and update velocities and locations (M2):
         vXOfSphere2 = vXOfSphere2 + (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * xOfSphere2 * TIMESTEP;
-        xOfSphere2 = xOfSphere2 + vXOfSphere2;
+        xOfSphere2 += lastVXOfSphere2 * TIMESTEP;
 
-        vYOfSphere2 = vYOfSphere2 + (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * yOfSphere2 * TIMESTEP;
-        yOfSphere2 = yOfSphere2 + vYOfSphere2;
+        vYOfSphere2 += (((2 * Gm) / powXYZ) - GM / (2 * powXY)) * yOfSphere2 * TIMESTEP;
+        yOfSphere2 += lastVYOfSphere2 * TIMESTEP;
 
 
         // Just separating the calculation steps for M3
@@ -188,8 +263,20 @@ public class Main extends Application {
         powXY = Math.pow(powXDiff + powYDiff + powZ3, 1.5);
 
         // Calculate and update velocities and locations (M3):
-        vZOfSphere3 = vZOfSphere3 + (-1) * ((4 * GM) / powXY) * zOfSphere3 * TIMESTEP;
-        zOfSphere3 = zOfSphere3 + vZOfSphere3;
+        vZOfSphere3 += (-1) * ((4 * GM) / powXY) * zOfSphere3 * TIMESTEP;
+        zOfSphere3 += lastVZOfSphere3 * TIMESTEP;
+
+        // Calculate number of cycles
+        if (Math.round(yOfSphere1) == 0 && !touchedYAxis) {
+            yCount++;
+            touchedYAxis = true;
+            if (yCount == 2) {
+                yCount = 0;
+                cycleCount++;
+            }
+        } else if (Math.round(yOfSphere1) != 0 && touchedYAxis == true) {
+            touchedYAxis = false;
+        }
 
         move(xOfSphere1, yOfSphere1, xOfSphere2, yOfSphere2, zOfSphere3);
     }
@@ -197,8 +284,8 @@ public class Main extends Application {
     private void move(double x1, double y1, double x2, double y2, double z) {
 
         // draw tracks of M1 & M2:
-        displayPane.getChildren().add(new Line(lastX1, lastY1, x1, y1));
-        displayPane.getChildren().add(new Line(lastX2, lastY2, x2, y2));
+//        displayPane.getChildren().add(new Line(lastX1, lastY1, x1, y1));
+//        displayPane.getChildren().add(new Line(lastX2, lastY2, x2, y2));
 
         // Move M1
         sphere1.translateXProperty().set(x1);
@@ -213,12 +300,39 @@ public class Main extends Application {
         sphere3.translateYProperty().set(yOfSphere3);
         sphere3.translateZProperty().set(z);
 
+        // Update the labels:
+        // Multiply Y and Z component by (-1) to match our views
+        currentSphere1XLabel.setText("Current X of M1: " + Round(xOfSphere1));
+        currentSphere1YLabel.setText("Current Y of M1: " + Round(yOfSphere1) * -1);
+        currentSphere2XLabel.setText("Current X of M2: " + Round(xOfSphere2));
+        currentSphere2YLabel.setText("Current Y of M2: " + Round(yOfSphere2) * -1);
+        currentSphere3ZLabel.setText("Current Z of M3: " + Round(zOfSphere3) * -1);
+
+        minX1 = Math.min(minX1, xOfSphere1);
+        maxX1 = Math.max(maxX1, xOfSphere1);
+        minX2 = Math.min(minX2, xOfSphere2);
+        maxX2 = Math.max(maxX2, xOfSphere2);
+        maxZ3 = Math.max(maxZ3, zOfSphere3);
+        minSphere1XLabel.setText("Min x of M1: " + Round(minX1));
+        maxSphere1XLabel.setText("Max x of M1: " + Round(maxX1));
+        minSphere2XLabel.setText("Min x of M2: " + Round(minX2));
+        maxSphere2XLabel.setText("Max x of M2: " + Round(maxX2));
+        maxSphere3ZLabel.setText("Max z of M3: " + Round(maxZ3));
+        cycleCountLabel.setText("Cycle count: " + cycleCount);
+
+        // Check camera properties:
 //        System.out.print("TranslateX: " + translate.getX());
 //        System.out.print(", TranslateY: " + translate.getY());
 //        System.out.print(", TranslateZ: " + translate.getZ());
 //        System.out.print(", RotateX: " + rotateX.getAngle());
 //        System.out.print(", RotateY: " + rotateY.getAngle());
 //        System.out.println(", RotateZ: " + rotateZ.getAngle());
+    }
+
+    private double Round(double value) {
+        BigDecimal bigDecimal = new BigDecimal(value);
+        bigDecimal = bigDecimal.round(new MathContext(3));
+        return bigDecimal.doubleValue();
     }
 
     public static void main(String[] args) {
